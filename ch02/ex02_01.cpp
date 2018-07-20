01a0533ad87a4c70a187983fb3d7160af04b8b82
hechengda <hechengda@gmail.com>
2018-07-20 14:08:57 +0800

    Modify Chapter 2 Exercise 1

--- ch02/ex02_01.cpp
+++ ch02/ex02_01.cpp
@@ -1,9 +1,10 @@
+// Solution for Chapter 2 Exercise 1
+
 #include "std_lib_facilities.h"
 
+// This program outputs a message
 int main()
 {
-    cout<<"Hello, programming!\n"
-        <<"Here we go!"
-        <<endl;
+    cout<<"Hello, programming!\nHere we go!\n";
     return 0;
 }
--- ch02/se02_01.cpp
+++
@@ -1,9 +0,0 @@
-// This program outputs the message ¡°Hello, World!¡± to the monitor
-#include "std_lib_facilities.h"
-
-// C++ programs start by executing the function main
-int main()
-{
-    cout<<"Hello, World!\n";  // output ¡°Hello, World!¡±
-    return 0;
-}
